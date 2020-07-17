8fe0288dbc17a20c5dde52fd5e0fa63321d39b81
Samet Erap <samet.erap@live.com>
2020-07-17 11:19:45 +0300

    project initialization

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { MapOptions, latLng, tileLayer } from 'leaflet';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  mapOptions: MapOptions;
+
+  ngOnInit() {
+    this.initializeMapOptions();
+  }
+
+  private initializeMapOptions() {
+    this.mapOptions = {
+      center: latLng(51.505, 0),
+      zoom: 12,
+      layers: [
+        tileLayer(
+          'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {
+            maxZoom: 18,
+            attribution: 'Map data © OpenStreetMap contributors'
+          })
+      ],
+    };
+  }
+
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    LeafletModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
